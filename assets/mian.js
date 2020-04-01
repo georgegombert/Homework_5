7d3134d078c62511403bceecdc1fa25058713747
george-g <georgegombert@gmail.com>
2020-03-31 21:01:11 -0600

    Updated styling and hourly containers created

--- assets/mian.js
+++ assets/mian.js
@@ -1,14 +1,18 @@
 let m = moment();
 const dateDisplay = m.format("[Today is ]dddd [the ] Do [of] MMMM");
 let currentTime = m.format("[The current time is] h[:]m[:]s[ ]a");
+const hourContainerNode = $("#hourContainer")
 
-console.log(dateDisplay , currentTime);
-
-let updateTime = setInterval(function(){
+//Initializing clock
+setInterval(function(){
     m = moment();
-    currentTime = m.format("[The current time is] h[:]mm[:]ss[ ]a");
-    console.log(currentTime);
+    currentTime = m.format("h[:]mm[:]ss[ ]a");
     $("#dateHeading").text(currentTime);
 },1000);
 
-updateTime();
+// creating divs to hold calendar events
+for(var i=0; i<24; i++){
+    const hourDivNode = $("<div>");
+    hourDivNode.attr({"class" : "hour-div", "data-hour" : i});
+    hourContainerNode.append(hourDivNode);
+}
