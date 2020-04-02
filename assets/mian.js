738b5bd7117ae734aad9037df40d4bf551f0bfc2
george-g <georgegombert@gmail.com>
2020-04-02 10:38:45 -0600

    Modal now taking information and printing to page

--- assets/mian.js
+++ assets/mian.js
@@ -1,7 +1,13 @@
+//Initializing moment.js
 let m = moment();
 const dateDisplay = m.format("[Today is ]dddd [the ] Do [of] MMMM");
 let currentTime = m.format("[The current time is] h[:]m[:]s[ ]a");
-const hourContainerNode = $("#hourContainer")
+const hourContainerNode = $("#hourContainer");
+let hourDivNode = $("#hourDiv");
+//Initializing moment.js
+
+//creating array storage from user input in modal
+let events = [];
 
 //Initializing clock
 setInterval(function(){
@@ -10,12 +16,36 @@ setInterval(function(){
     $("#dateHeading").text(currentTime);
 },1000);
 
-// Creating divs to hold calendar events
-for(var i=0; i<24; i++){
-    const hourDivNode = $("<div>");
-    hourDivNode.attr({"class" : "hour-div", "data-hour" : i});
-    let hourMark = m.set({'hour': i, 'minute': 0, 'second': 0});
-    hourMark = hourMark.format('h[:]mm a');
-    hourDivNode.text(hourMark);
-    hourContainerNode.append(hourDivNode);
+
+function printEvents(){
+    for(event in events){
+        hourDivNode.text(JSON.stringify(events[event]));
+    }
 }
+
+$("#eventButton").click(function(){
+    events.push({
+        eventName : $("#eventName").val().trim(),
+        startHour : $("#startHour").val(),
+        startMinute : $("#startMinute").val(),
+        endHour : $("#endHour").val(),
+        endMinute : $("#endMinute").val(),
+        eventDescription : $("#eventDescription").val().trim()
+    })
+    console.log(events);
+    printEvents();
+})
+
+
+
+
+// Creating divs to hold calendar events used to generate html then commented out
+// I left this in so you could see how I made the hour divs
+// for(var i=0; i<24; i++){
+//     const hourDivNode = $("<div>");
+//     hourDivNode.attr({"class" : "hour-div", "data-hour" : i});
+//     let hourMark = m.set({'hour': i, 'minute': 0, 'second': 0});
+//     hourMark = hourMark.format('h[:]mm a');
+//     hourDivNode.text(hourMark);
+//     hourContainerNode.append(hourDivNode);
+// }
